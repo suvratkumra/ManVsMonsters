df02f91236c6e6bb3c66f6f74b0c5032f1e6c1ba
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2022-08-31 13:32:08 -0600

    Removing old files

--- ManVsMonsters/Intermediate/Build/Win64/ManVsMonstersEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "D:/Unreal Engine Projects/ManVsMonsters/ManVsMonsters/Intermediate/Build/Win64/ManVsMonstersEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- ManVsMonsters/Source/ManVsMonsters/ManVsMonsters.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ManVsMonsters.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ManVsMonsters, "ManVsMonsters" );
